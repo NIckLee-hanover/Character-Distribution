bf036b9e0134b4a81b10b8674c7d997ce803f016
NIckLee-hanover <42844776+NIckLee-hanover@users.noreply.github.com>
2018-09-30 14:06:38 -0400

    Updated from Brython Server: 9/30/2018 2:06:37 PM

--- distribution.py
+++ distribution.py
@@ -1,7 +1,7 @@
 """
 distribution.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Nick Lee
+Credit: stackoverflow for 2 things so far
 
 Assignment:
 
@@ -36,3 +36,46 @@ Notice about this example:
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 """
+# convert all text to lower case  //
+# asign each char a number
+# group numbers 
+# sort groups
+# sort groups again
+
+import string
+
+string1 = input("Please enter a string of text (the bigger the better): ")
+print ('The distribution of characters in "', str(string1), ". is:")
+string1 = list(string1)
+lower =  string.ascii_lowercase
+upper =  string.ascii_uppercase
+
+for i in range(len(string1)): # string is now all lowercase
+    letter = string1[i]
+    for j in range(26):
+        if letter == upper[j]:
+            string1[i] = lower[j]
+        string1[i] = str(j)
+    
+"""
+for i in range(len(string1)):
+    letter = string1[i]
+    for j in range(26):
+        if letter == lower[j]:
+            string1[i] = str(j)
+"""
+string1 = ''.join(string1) # joins list back into string with the help of overflow
+
+print(str(string1)) 
+
+
+
+
+
+
+
+
+
+
+
+
